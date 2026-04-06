526c5997f92967b461521a2053307639e404a637
aa0911292144-dot <aa0911292144@gmail.com>
2026-04-07 00:18:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-// 註冊 GSAP ScrollTrigger
+// 初始化 Swiper 輪播
+var swiper = new Swiper(".painSwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true, // 無限循環
+    autoplay: {
+        delay: 5000, // 每5秒自動換圖
+        disableOnInteraction: false,
+    },
+    effect: "fade", // 使用高質感的淡入淡出效果
+});
+
+// 註冊 GSAP 動畫
 gsap.registerPlugin(ScrollTrigger);
 
-// 簡單的淡入浮現
+// 元素進入視窗時的浮現動畫
 gsap.utils.toArray('.gs-reveal').forEach(function(elem) {
     gsap.fromTo(elem, 
         { y: 40, opacity: 0 }, 
@@ -12,7 +30,7 @@ gsap.utils.toArray('.gs-reveal').forEach(function(elem) {
     );
 });
 
-// 左側滑入 (產品圖)
+// 左側滑入
 gsap.fromTo('.gs-reveal-left', 
     { x: -50, opacity: 0 }, 
     {
@@ -21,7 +39,7 @@ gsap.fromTo('.gs-reveal-left',
     }
 );
 
-// 右側滑入 (成分表)
+// 右側滑入
 gsap.fromTo('.gs-reveal-right', 
     { x: 50, opacity: 0 }, 
     {
@@ -29,15 +47,3 @@ gsap.fromTo('.gs-reveal-right',
         scrollTrigger: { trigger: '.gs-reveal-right', start: "top 80%" }
     }
 );
-
-// 痛點卡片交錯浮現
-ScrollTrigger.create({
-    trigger: ".pain-grid",
-    start: "top 80%",
-    onEnter: () => {
-        gsap.fromTo(".gs-reveal-up", 
-            { y: 40, opacity: 0 },
-            { y: 0, opacity: 1, duration: 0.8, stagger: 0.15, ease: "power2.out" }
-        );
-    }
-});
